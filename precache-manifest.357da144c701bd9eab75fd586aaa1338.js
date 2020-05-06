d145f1125630c98c2fb9fb1bdcd75e6128dc1104
Allen Shin <geeuho@gmail.com>
2020-05-05 22:08:26 -0700

    Updates

---
+++ precache-manifest.357da144c701bd9eab75fd586aaa1338.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df5b6c785df8cadb4176f4e4d62d134",
+    "url": "/Nutrition_Tracker_2.0_frontend/index.html"
+  },
+  {
+    "revision": "ee982321b0826a43f645",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "0ed7d24ab215fe80a15b",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/css/main.25db4726.chunk.css"
+  },
+  {
+    "revision": "ee982321b0826a43f645",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/js/2.e15ad03a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/js/2.e15ad03a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ed7d24ab215fe80a15b",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/js/main.9b4ea4dd.chunk.js"
+  },
+  {
+    "revision": "9fa641999f454f167a59",
+    "url": "/Nutrition_Tracker_2.0_frontend/static/js/runtime-main.3d872075.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Nutrition_Tracker_2.0_frontend/precache-manifest.357da144c701bd9eab75fd586aaa1338.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Nutrition_Tracker_2.0_frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
